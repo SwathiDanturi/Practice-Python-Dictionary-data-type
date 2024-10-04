a97dea5c048e77f51d6465e0a3ba39c49dcfba30
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-10-04 17:02:16 -0400

    write test cases for find_programmers

--- test_find_programmers.py
+++ test_find_programmers.py
@@ -7,3 +7,35 @@ Developer: Swathi
 Collaborator(s): Ruchitha
 Date: 10/03/2024
 """
+
+import pytest
+from programming_languages import ProgrammingLanguages
+
+
+def test_find_programmers_one_programmer():
+    """
+    Test find_programmers with one programmer
+    proficient in the given input language
+    """
+    programmers = ['Bill', 'Joan']
+    languages = [['C++', 'Python'], ['C++']]
+    pl_obj = ProgrammingLanguages(programmers, languages)
+    expected = ['Bill']
+    actual = pl_obj.find_programmers('Python')
+    assert actual == expected
+
+
+def test_find_programmers_more_than_one_programmer():
+    """
+    Test find_programmers with more than one programmer
+    proficient in the given input language
+    """
+    programmers = ['Bill', 'Joan']
+    languages = [['C++', 'Python'], ['C++']]
+    pl_obj = ProgrammingLanguages(programmers, languages)
+    expected = [['Bill', 'Joan']]
+    actual = pl_obj.find_programmers('C++')
+    assert actual == expected
+
+
+pytest.main()
